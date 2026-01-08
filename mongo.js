cbc2232ef9b206875af18b72d9f115060c5a8947
Tzvi H <16guitar@gmail.com>
2026-01-08 10:29:41 -0600

    update

--- mongo.js
+++ mongo.js
@@ -1,38 +1,22 @@
 const mongoose = require("mongoose");
-
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.zjo4nqe.mongodb.net/?appName=Cluster0`;
+const { MONGODB_URI } = require("./utils/config");
+const Note = require("./models/note");
 
 mongoose.set("strictQuery", false);
+mongoose.connect(MONGODB_URI, { family: 4 });
 
-mongoose.connect(url, { family: 4 });
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const note1 = new Note({
+  content: "HTML is easy",
+  important: true,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const note2 = new Note({
+  content: "Css is easy",
+  important: false,
+});
 
-// const note = new Note({
-//   content: "HTML is easy",
-//   important: true,
-// });
-
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
+note1.save().then(() => {
+  note2.save().then(() => {
+    mongoose.connection.close();
   });
-  mongoose.connection.close();
 });
